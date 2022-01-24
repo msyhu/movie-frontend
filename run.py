d1a701ae06764c3ad4b4b2a90f018658cba5b90c
msyhu <msyhu@korea.ac.kr>
2022-01-24 17:22:38 +0900

    init

---
+++ run.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import datetime, requests
+
+app = Flask(__name__)
+baseurl = 'http://172.30.5.139:30007/movies/name/'
+
+@app.route("/")
+def index():
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route("/details")
+def details():
+    name = request.args.get('name', default = '*', type = str)
+    print(name)
+    fullurl = baseurl + name
+    response = requests.get(fullurl)
+    movie = response.json()
+    print(movie)
+    return render_template('details.html', movie=movie)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
+ 
